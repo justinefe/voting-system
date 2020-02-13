1175933fb23688fdcca64bfe86e6ce7629fcca4e
Justin <efejustin3@gmail.com>
2020-02-13 16:52:02 +0100

    bg(working hard to fix bug) [fixes#171257501]

--- src/config/config.js
+++ src/config/config.js
@@ -1,29 +1,18 @@
 import { config } from 'dotenv';
 
-
 config();
-console.log(process.env.PASSWORD, process.env.USER, process.env.DEV_DATABASE, '==== Password');
 
 module.exports = {
   development: {
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DEV_DATABASE,
-    host: '127.0.0.1',
-    dialect: 'mysql' 
+    use_env_variable: 'DATABASE_URL_DEV',
+    dialect: 'postgres'
   },
   test: {
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.TEST_DATABASE,
-    host: '127.0.0.1',
-    dialect: 'mysql'  
+    use_env_variable: 'DATABASE_URL_TEST',
+    dialect: 'postgres'
   },
   production: {
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.PRODUCTION_DATABASE,
-    host: '127.0.0.1',
-    dialect: 'mysql'
+    use_env_variable: 'DATABASE_URL_PROD',
+    dialect: 'postgres'
   }
 };
