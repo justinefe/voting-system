501a6bd7c523f0bc742c24299adcdb978db8b59a
Justin <efejustin3@gmail.com>
2020-02-13 08:55:20 +0100

    Untrack files in .gitignore

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-// import http from 'http';
-// import app from './app';
-// const server = http.createServer(app);
-// const port = process.env.PORT || 3020;
-// server.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-// export default server;
-"use strict";
